86cc2637de5123c56bea46a26d37a9e1b87613d3
Diego Vargas <diego.v.u21@gmail.com>
2024-09-01 02:10:16 -0400

    Revert "Integracion dependencias AstroDB"
    This reverts commit 03b7a2e48bff8bfdae7ae781f2fd8a5d34937b40.

--- code/astro.config.mjs
+++ code/astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import db from '@astrojs/db';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()]
+  integrations: [tailwind()]
 });
